6002b24d7cc096f343b3252a7c7f3a8319cc0a75
Simon-mac <zeroing_one@163.com>
2019-02-19 18:51:10 +0800

    小修改

---
+++ .vuepress/config.js
@@ -0,0 +1,67 @@
+module.exports = {
+    title: "Simon's Blog",
+    themeConfig: {
+        nav: [
+            {
+                text: '前端',
+                items: [
+                    {
+                        text: 'js',
+                        link: '/js/'
+                    }
+                ]
+            },
+            {
+                text: '后端',
+                items: [
+                    {
+                        text: 'mysql',
+                        link: '/mysql/'
+                    },
+                    {
+                        text: 'elasticsearch',
+                        link: '/elasticsearch/'
+                    },
+                    {
+                        text: 'redis',
+                        link: '/redis/'
+                    },
+                    {
+                        text: 'nginx',
+                        link: '/nginx/'
+                    },
+                    {
+                        text: 'swoole',
+                        link: '/swoole/'
+                    },
+                    {
+                        text: '直播入门',
+                        link: '/live/'
+                    }
+                ]
+            }
+        ],
+        sidebar: {
+            '/js/': [
+                '',
+                'let',
+                'syntaxSugar',
+                'object',
+                'arrowFunction',
+                'vituralDom',
+                'promise',
+                'class',
+            ],
+            '/live/': [''],
+            '/redis/': [''],
+            '/mysql/': [
+                '',
+                'logSystem'
+            ],
+            '/elasticsearch/': [''],
+            '/swoole/': [''],
+            '/nginx/': [''], 
+            '/': ['']
+        }
+    }
+}
